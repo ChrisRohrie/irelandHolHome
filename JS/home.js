dd7ebdf64580ac9e21147e6da8ce01368c995a30
Chris Rothrie <crothrie@hotmail.com>
2023-12-14 19:47:46 +0000

    JS developement for home

--- JS/home.js
+++ JS/home.js
@@ -1,22 +1,46 @@
-//Variables
+/* VARIABLES */
 
+//Page
 let table = document.getElementById("bookingsTable");
 
+//Form
 let fromInput;
 let toInput;
-let daysInput;
 let whoInput;
+let numRooms
+
+//Class
+
+let fromDate;
+let toDate;
+let days;
+
+/* MAIN FUNCTIONS */
 
 function clearForm() {
     document.getElementById("bookingForm").reset();
 }
 
 function addBooking() {
+    
+    //get inputs
     fromInput = document.getElementById("from").value;
+    fromDate = Date.parse(fromInput);
+
     toInput = document.getElementById("to").value;
-    console.log(typeof(fromInput));
-    console.log(fromInput);
-    daysInput = toInput - fromInput;
-    console.log(daysInput);
-    whoInput = document.getElementById("who");
+    toDate = Date.parse(toInput);
+
+    whoInput = document.getElementById("who").value;
+    console.log(typeof whoInput);
+    console.log(whoInput);
+
+    numRooms = document.getElementById("rooms").value;
+    numRooms = parseInt(numRooms);
+    console.log(typeof numRooms);
+    console.log(numRooms);
+
+    //calc diff between dates to get trip length
+    days = toDate - fromDate;
+    days = Math.round(days / (1000 * 3600 * 24));
+
 }

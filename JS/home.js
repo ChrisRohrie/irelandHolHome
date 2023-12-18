30eafc2726b9a957b4456f74a1604657bb20d1d2
Chris Rothrie <crothrie@hotmail.com>
2023-12-18 19:42:38 +0000

    Home page updates including error checking enhancements

--- JS/home.js
+++ JS/home.js
@@ -2,45 +2,95 @@
 
 //Page
 let table = document.getElementById("bookingsTable");
+let form = document.getElementById("bookingForm");
 
-//Form
+//Form input vars
 let fromInput;
 let toInput;
 let whoInput;
-let numRooms
+let numRoomsInput;
 
-//Class
+//local vars
 
 let fromDate;
 let toDate;
 let days;
+let today;
 
-/* MAIN FUNCTIONS */
+/* FUNCTIONS */
 
+//Utility
+//prevent form from auto submit
+function submitForm(event){
+
+    //Preventing page refresh
+    event.preventDefault();
+ }
+
+ form.addEventListener('submit', submitForm);
+
+ //Main
 function clearForm() {
     document.getElementById("bookingForm").reset();
 }
 
 function addBooking() {
     
+    //remove old booings TBC
+
     //get inputs
     fromInput = document.getElementById("from").value;
     fromDate = Date.parse(fromInput);
+    console.log(fromInput);
+    console.log(fromDate);
+    if(fromInput==="") { console.log("TRUE")}
 
     toInput = document.getElementById("to").value;
     toDate = Date.parse(toInput);
 
     whoInput = document.getElementById("who").value;
-    console.log(typeof whoInput);
     console.log(whoInput);
 
-    numRooms = document.getElementById("rooms").value;
-    numRooms = parseInt(numRooms);
-    console.log(typeof numRooms);
-    console.log(numRooms);
+    numRoomsInput = document.getElementById("rooms").value;
+    numRoomsInput = parseInt(numRoomsInput);
 
     //calc diff between dates to get trip length
     days = toDate - fromDate;
     days = Math.round(days / (1000 * 3600 * 24));
 
+    //validation of params   
+    try {
+        today  = Date.now();
+        if(fromDate-today<0) throw "From date is in the past";
+        if(days<0) throw "To date needs to be after from date";
+        if(days>90) throw "Your trip is too long";
+        if(numRoomsInput<1) throw "Number of rooms is too low";
+        if(numRoomsInput>5) throw "Number of rooms is too high";
+        if(fromInput==="") throw "Missing from date";
+        if(toInput==="") throw "Missing to date";
+        if(whoInput==="") throw "Missing who is staying";
+        if(numRoomsInput==="") throw "Missing number of rooms";
+    }   
+    catch(err) {
+        alert(err);
+        return;
+    } 
+
+    
+    //update the table
+    var rowOutput = table.insertRow();
+
+    var fromOutput = rowOutput.insertCell(0);
+    var toOutput = rowOutput.insertCell(1);
+    var daysOutput = rowOutput.insertCell(2);
+    var whoOutput = rowOutput.insertCell(3);
+    var roomsOutput = rowOutput.insertCell(4);
+
+    fromOutput.innerHTML = fromInput;
+    toOutput.innerHTML = toInput;
+    daysOutput.innerHTML = days;
+    whoOutput.innerHTML  = whoInput;
+    roomsOutput.innerHTML = numRoomsInput;
+
+    clearForm();
 }
